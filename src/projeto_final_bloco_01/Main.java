b34b97088a3cfcfd057a621737db94546d31131d
DouglasIde <douglasymide@gmail.com>
2024-06-20 09:54:15 -0300

    Criando a classe Main(Menu) e importando o Scanner

---
+++ src/projeto_final_bloco_01/Main.java
@@ -0,0 +1,13 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
