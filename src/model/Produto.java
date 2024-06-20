f6f00546f2a511df01a7407749fbde45e02b506c
DouglasIde <douglasymide@gmail.com>
2024-06-20 10:25:28 -0300

    Criando o pacote MODEL e a Classe PRODUTO com o construtor e as informaçoes

---
+++ src/model/Produto.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Produto {
+	
+	private String nome, cor;
+	private double preco;
+	private char tamanho;
+	
+	public Produto(String nome, String cor, double preco, char tamanho) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.preco = preco;
+		this.tamanho = tamanho;
+	}
+	
+	
+}
