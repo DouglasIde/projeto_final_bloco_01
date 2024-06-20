4c9e6d0886541b2e169613729bde71c51690f69b
DouglasIde <douglasymide@gmail.com>
2024-06-20 12:16:35 -0300

    Criando o método CADASTRAR

--- src/controller/ProdutoController.java
+++ src/controller/ProdutoController.java
@@ -1,5 +1,38 @@
 package controller;
 
-public class ProdutoController {
+import java.util.ArrayList;
 
+import model.Produto;
+import repository.IProdutosRepository;
+
+
+public class ProdutoController implements IProdutosRepository{
+
+	private ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
+
+	@Override
+	public void cadastrar(Produto produto) {
+		listaProdutos.add(produto);
+		System.out.println("O produto " + produto.getNome() + " foi Criado com Sucesso!");
+	}
+
+	@Override
+	public void listarProdutos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarProduto(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletarProduto(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
