43cb9ae088d0cb2b76aba09880d4fd1f4f498411
DouglasIde <douglasymide@gmail.com>
2024-06-20 09:58:18 -0300

    Criando a descriçao do Menu junto com o laço WHILE - Task 1

--- src/projeto_final_bloco_01/Main.java
+++ src/projeto_final_bloco_01/Main.java
@@ -7,6 +7,30 @@ public class Main {
 	public static void main(String[] args) {
 		
 	Scanner scanner = new Scanner(System.in);
+	
+	int opcao;
+	
+	while(true) {
+		System.out.println("""
+				
+				************************
+				
+				CENTOURO
+				
+				************************
+				Selecione uma das opções abaixo:
+				
+				1 - Cadastrar Produto
+				2 - Listar todos os Produtos
+				3 - Atualizar um Produto
+				4 - Apagar um Produto
+				5 - Sair
+				
+				Digite a sua opção:
+				""");
+		opcao = scanner.nextInt();
+	}
+	
 
 	}
 
